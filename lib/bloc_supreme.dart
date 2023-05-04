fcf46dae0526a9ddcb6d712ca4b1dd2a30a20849
ERGataullin <ERGataullin2000@gmail.com>
2023-05-04 21:23:02 +0300

    Basic implementation

--- lib/bloc_supreme.dart
+++ lib/bloc_supreme.dart
@@ -1,7 +1,3 @@
 library bloc_supreme;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/bloc.dart';
---
+++ lib/src/bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:synchronized/synchronized.dart';
+
+abstract class Bloc<S> {
+  Bloc(S initialState) {
+    _stateController.add(initialState);
+  }
+
+  final StreamController<S> _stateController = StreamController.broadcast();
+
+  final Lock _lock = Lock();
+
+  Stream<S> get state => _stateController.stream;
+
+  Future<void> execute(Event<S> event) {
+    event._bloc = this;
+    return _lock.synchronized(event);
+  }
+
+  void close() {
+    _stateController.close();
+  }
+}
+
+abstract class Event<S> {
+  late Bloc<S> _bloc;
+
+  FutureOr<void> proceed();
+
+  void onError(dynamic error, StackTrace stackTrace) {}
+
+  FutureOr<void> call() {
+    return Future.value(proceed()).catchError(onError);
+  }
+
+  void emit(S state) {
+    _bloc._stateController.add(state);
+  }
+}
